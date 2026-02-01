2ce8812c2fc275278698da003b1c4013b370c0b6
Ananya <ananya162005@gmail.com>
2026-02-01 23:31:01 +0530

    Add files via upload

---
+++ for loop (element based).py
@@ -0,0 +1,26 @@
+#FOR LOOP WITH LIST (element based)
+
+#1. print all elements of a list 
+nums = [10,-5,60,30,-20,80]
+for num in nums:
+    print(num)
+
+#2. print elements multiplied by 2 
+for num in nums:
+    print(num*2)
+    
+#3. print onli positive numbeers 
+for num in nums:
+    if num>0:
+        print(num)
+        
+#4. print onli negative nub
+for num in nums:
+    if num<0:
+        print(num)
+
+#5. print numbs greater than 50:
+for num in nums:
+    if num>50:
+        print(num)
+        
